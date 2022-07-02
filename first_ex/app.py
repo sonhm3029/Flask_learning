d40ff24640446ebd76aa8348e5bb9b3e7559a5a5
Son Hoang <hoangminhson29102001@gmail.com>
2022-07-02 23:31:21 +0700

    Thêm info

--- first_ex/app.py
+++ first_ex/app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
+
+# app.add_url_rule('/', 'hello',hello_world)
+
+@app.route('/home')
+def home():
+    return 'Home'
+
+@app.route('/profile/<path:id>')
+def profile(id):
+    return 'Hello %s' % id
+
+@app.route('/admin')
+def admin():
+    return 'Hello admin'
+
+@app.route('/guest/<guest>')
+def guest(guest):
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def user(name):
+    if name == 'admin':
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('guest', guest=name))
+
 
 if __name__=="__main__":
     app.run(debug=True)
